2f313aa0c235bc98e63b7cd0c34b7f8296f953c6
Dave Burnison <DaveBurnisonMS@GitHub.com>
2020-03-28 16:22:43 -0700

    Replaced FabrikamFiber with PartsUnlimited.

--- PartsUnlimited-aspnet45/test/PartsUnlimited.SeleniumTests/PartsUnlimitedTests.cs
+++ PartsUnlimited-aspnet45/test/PartsUnlimited.SeleniumTests/PartsUnlimitedTests.cs
@@ -4,7 +4,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Linq;
 
-namespace FabrikamFiber.SeleniumTests
+namespace PartsUnlimited.SeleniumTests
 {
     [TestClass]	
     public class PartsUnlimitedTests
