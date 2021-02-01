549aa8ef71ad274523c386961f87ef38e6726f2c
Dave Burnison <DaveBurnisonMS@github.com>
2021-02-01 08:52:52 -0800

    Update the subsidiary name in _Layout.cshtml  This fixes AB#4696 (#89)
    * This fixes AB#4698
    
    * Update the subsidiary name. This Fixes AB#4697

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Areas/Admin/Controllers/StoreManagerController.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Areas/Admin/Controllers/StoreManagerController.cs
@@ -185,7 +185,7 @@ namespace PartsUnlimited.Areas.Admin.Controllers
         //
         // POST: /StoreManager/RemoveProduct/5
         [HttpPost, ActionName("RemoveProduct")]
-        public async Task<ActionResult> RemoveProductConfirmed(int id)  // Is there a security issue here? (Demo update 1/25/21)
+        public async Task<ActionResult> RemoveProductConfirmed(int id)  // Do we have a security issue? (Demo update 2/1/21)
         {
             Product product = db.Products.Where(a => a.ProductId == id).FirstOrDefault();
 
