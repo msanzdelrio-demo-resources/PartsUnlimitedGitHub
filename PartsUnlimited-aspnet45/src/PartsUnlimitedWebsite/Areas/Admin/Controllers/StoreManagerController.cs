8399c1228f7a12336a52f4abaec11679ae83b4e3
Dave Burnison <DaveBurnisonMS@github.com>
2021-03-24 18:09:22 -0700

    Update the subsidiary name. This Fixes AB#4906 (#100)
    * Update the Busienss Unit tag. This Fixes AB#4908
    
    * Update the subsidiary name. This Fixes AB#4907
    
    * This Fixes AB#4909

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Areas/Admin/Controllers/StoreManagerController.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Areas/Admin/Controllers/StoreManagerController.cs
@@ -185,7 +185,7 @@ namespace PartsUnlimited.Areas.Admin.Controllers
         //
         // POST: /StoreManager/RemoveProduct/5
         [HttpPost, ActionName("RemoveProduct")]
-        public async Task<ActionResult> RemoveProductConfirmed(int id)  // Do we have a security issue here? (Demo update 3/13/2021)
+        public async Task<ActionResult> RemoveProductConfirmed(int id)  // We may have a security issue here. (Demo update 3/24/2021 5:43 PM)
         {
             Product product = db.Products.Where(a => a.ProductId == id).FirstOrDefault();
 
