414e3ddcd4261b0be15f9f208918f5dc2c23a1a8
Dave Burnison <DaveBurnisonMS@github.com>
2021-09-20 11:10:50 -0700

    Update subsidiary ref to Contoso.  (#120) This Fixes AB#5098
    - Update the subsidiary name. This Fixes AB#5099
    - Update StoreController.cs This Fixes AB#5101
    - Update the Busienss Unit tag. This Fixes AB#5100

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Areas/Admin/Controllers/StoreManagerController.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Areas/Admin/Controllers/StoreManagerController.cs
@@ -185,7 +185,7 @@ namespace PartsUnlimited.Areas.Admin.Controllers
         //
         // POST: /StoreManager/RemoveProduct/5
         [HttpPost, ActionName("RemoveProduct")]
-        public async Task<ActionResult> RemoveProductConfirmed(int id)  // Is there a security issue here? (Demo update 8/18/2021)
+        public async Task<ActionResult> RemoveProductConfirmed(int id)  // There might be a security issue here, (Demo update 9/20/2021)
         {
             Product product = db.Products.Where(a => a.ProductId == id).FirstOrDefault();
 
