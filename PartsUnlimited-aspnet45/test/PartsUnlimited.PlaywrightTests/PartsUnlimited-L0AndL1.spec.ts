bdc667db9eab5402e1cafde973eac02c6a16d56d
Dave Burnison <DaveBurnisonMS@github.com>
2022-03-13 14:55:40 -0700

    Use Levels 1 & 2 instead of 0 & 1

--- PartsUnlimited-aspnet45/test/PartsUnlimited.PlaywrightTests/PartsUnlimited-L0AndL1.spec.ts
+++ PartsUnlimited-aspnet45/test/PartsUnlimited.PlaywrightTests/PartsUnlimited-L0AndL1.spec.ts
@@ -7,34 +7,34 @@ test.beforeEach(async ({ page }) => {
   await page.goto(baseURL);
 });
 
-test('Verify Subsidiary Name On Home Page @LEVEL_0', async ({ page }) => {
+test('Verify Subsidiary Name On Home Page @LEVEL_1', async ({ page }) => {
 
     // Home Page
     await expect(page.locator('text=' + subsidiaryName).first()).toBeVisible();
 });
   
-test('Verify Subsidiary Name On Store Page @LEVEL_0', async ({ page }) => {
+test('Verify Subsidiary Name On Store Page @LEVEL_1', async ({ page }) => {
 
     // Store Page
     await page.goto(baseURL + '/Store');
     await expect(page.locator('text=' + subsidiaryName).first()).toBeVisible();
 });
 
-test('Verify Subsidiary Name On Login Page @LEVEL_0', async ({ page }) => {
+test('Verify Subsidiary Name On Login Page @LEVEL_1', async ({ page }) => {
 
     // Login Page
     await page.goto(baseURL + '/Login');
     //  await expect(page.locator('text=' + subsidiaryName).first()).oBeVisible();
 });
 
-test('Verify Subsidiary Name On ShoppingCart Page @LEVEL_0', async ({ page }) => {
+test('Verify Subsidiary Name On ShoppingCart Page @LEVEL_1', async ({ page }) => {
 
     // Shopping Page
     await page.goto(baseURL + '/ShoppingCart');
     await expect(page.locator('text=' + subsidiaryName).first()).toBeVisible();
 });
 
-test('Verify Subsidiary Name On Product Pages @LEVEL_1', async ({ page }) => {
+test('Verify Subsidiary Name On Product Pages @LEVEL_2', async ({ page }) => {
     // Each Product Category Page
     for (const item of categories) {
         await page.goto(baseURL + '/Store/Browse?CategoryId=' + item);
