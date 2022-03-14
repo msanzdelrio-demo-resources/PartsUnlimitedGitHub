39e402da32a8e4b50ffb1110bf558aea2db9af7c
Dave Burnison <DaveBurnisonMS@github.com>
2022-03-13 19:55:20 -0700

    Features/playwright1st codegen tests (#172)
    * Add a test via Codegen
    
    * Add the 'Login and Manage Account' test

--- PartsUnlimited-aspnet45/test/PartsUnlimited.PlaywrightTests/PartsUnlimited-L1AndL2.spec.ts
+++ PartsUnlimited-aspnet45/test/PartsUnlimited.PlaywrightTests/PartsUnlimited-L1AndL2.spec.ts
@@ -41,6 +41,62 @@ test.describe('Level 1 Tests', () => {
 // Level 2 Tests
 test.describe('Level 2 Tests', () => {
 
+    // CodeGen
+    test('Search Test @LEVEL_2', async ({ page }) => {
+        // This function was created by using 'npx playwright codegen' and:
+        // - Searching for 'oil'
+        // - Clicking on the itme labeld 'Synthetic Engin Oil'
+        // - Verifying that the title of the page said 'Synthetic Engin Oil' 
+        // - Copy/Paste the code into this function. 
+
+        // Fill [placeholder="Keyword"] with 'oil'
+        await page.locator('[placeholder="Keyword"]').fill('oil');
+      
+        // Press Enter
+        await page.locator('[placeholder="Keyword"]').press('Enter');
+
+        // Click img[alt="Synthetic\ Engine\ Oil\ zoom"]
+        await expect(page.locator('img[alt="Synthetic\\ Engine\\ Oil\\ zoom"]').first()).toBeVisible();
+        await page.locator('img[alt="Synthetic\\ Engine\\ Oil\\ zoom"]').click();
+
+        // Click text=Synthetic Engine Oil
+        await expect(page.locator('text=Synthetic Engine Oil').first()).toBeVisible();
+    });    
+  
+
+    test('Login and Manage Account @LEVEL_2', async ({ page }) => {
+
+        // Click text=Log in
+        await page.locator('text=Log in').click();
+        await expect(page).toHaveURL(baseURL + '/Account/Login');
+
+        // Click [placeholder="Email\ Address"]
+        await page.locator('[placeholder="Email\\ Address"]').click();
+
+        // Fill [placeholder="Email\ Address"]
+        await page.locator('[placeholder="Email\\ Address"]').fill('abc@outlook.com');
+
+        // Click [placeholder="Password"]
+        await page.locator('[placeholder="Password"]').click();
+
+        // Fill [placeholder="Password"]
+        await page.locator('[placeholder="Password"]').fill('F@keP@55word');
+
+        // Click text=Login
+        await page.locator('text=Login').click();
+        await expect(page).toHaveURL(baseURL);
+
+        // Click text=Profile >> b
+        await page.locator('text=Profile >> b').click();
+
+        // Click #collapse-header-nav >> text=Manage Account
+        await page.locator('#collapse-header-nav >> text=Manage Account').click();
+        await expect(page).toHaveURL(baseURL +  '/Manage');
+
+        // Click text=Change your account settings
+        await expect(page.locator('text=Change your account settings').first()).toBeVisible();
+    });
+
     test('Verify Subsidiary Name On Product Pages @LEVEL_2', async ({ page }) => {
         // Each Product Category Page
         for (const item of categories) {
@@ -48,7 +104,6 @@ test.describe('Level 2 Tests', () => {
             await expect(page.locator('text=' + subsidiaryName).first()).toBeVisible();
         }
     });
+
 });
 
-
-  
