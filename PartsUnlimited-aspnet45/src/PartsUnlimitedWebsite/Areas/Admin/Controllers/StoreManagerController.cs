52c0af45ff160a5e4ebdd2ac83ca0c637fd72c60
Dave Burnison <DaveBurnisonMS@github.com>
2022-12-03 14:06:24 -0500

    Features/update subsidiary name to Fabrikam. This Fixes AB#5386 (#231)
    Features/update subsidiary name to Fabrikam. This Fixes AB#5386
    - Update the subsidiary name. This Fixes AB#5387
    - Update the Business Unit tag. This Fixes AB#5388
    - Update StoreController.cs This Fixes AB#5389
    - Remove extensitions that are generating prompts/warnings.

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Areas/Admin/Controllers/StoreManagerController.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Areas/Admin/Controllers/StoreManagerController.cs
@@ -185,7 +185,7 @@ namespace PartsUnlimited.Areas.Admin.Controllers
         //
         // POST: /StoreManager/RemoveProduct/5
         [HttpPost, ActionName("RemoveProduct")]
-        public async Task<ActionResult> RemoveProductConfirmed(int id)  // Contoso Demo update 2022 11 07
+        public async Task<ActionResult> RemoveProductConfirmed(int id)  // Fabrikam Demo update 2022 12 03
         {
             Product product = db.Products.Where(a => a.ProductId == id).FirstOrDefault();
 
